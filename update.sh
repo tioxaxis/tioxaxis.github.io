a5c9aaf14124936e967d190ddb2f1a7b82fb2524
TioX <gregory.c.dobson@gmail.com>
2021-01-12 16:29:24 -0500

    Upgrading the default to v1.1

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 VERSION=$1
-[ -z "$VERSION" ] && echo "Usage: ./change.sh <VERSION> [<COMMIT>]" && exit 1
+[ -z "$VERSION" ] && echo "Usage: ./update.sh <VERSION>" && exit 1
 cd `dirname $0`
 
 # Modify an existing branch/submodule to point to a different commit (or HEAD by default).
