5024add4c781b5caa9c42764ca63f3fc5ece0dfb
Nathan Dobson <nadobson@mac.com>
2021-03-05 16:44:16 -0500

    Partial implementation of update.sh for different branches.

--- update.sh
+++ update.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 set -e
-VERSION=$1
-[ -z "$VERSION" ] && echo "Usage: ./change.sh <VERSION> [<COMMIT>]" && exit 1
+SUBMODULE=$1
+BRANCH=$2
+[ -z "$VERSION" ] \
+  && echo "Usage: ./update.sh <SUBMODULE> [<BRANCH>]" \
+  && echo \
+  && echo "Update <SUBMODULE> to the current HEAD commit from <BRANCH>. If <BRANCH> is omitted, " \
+  && echo "it defaults to the current branch of <SUBMODULE>" \
+  && exit 1
 cd `dirname $0`
 
-# Modify an existing branch/submodule to point to a different commit (or HEAD by default).
-git submodule update --remote --merge $VERSION
-git add $VERSION
-git commit -m "Updating version $VERSION"
+git config --file=.gitmodules submodule.$SUBMODULE.branch $BRANCH
+git submodule sync
+git submodule update --remote --merge $SUBMODULE
+git add $SUBMODULE
+git commit -m "Updating submodule $SUBMODULE to HEAD of $BRANCH"
 git push
